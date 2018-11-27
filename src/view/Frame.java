31f5bbf4d8e2d998a57389f5a02a44b9e17fba76
Jensen Judkins <thesmelliestman@gmail.com>
2018-11-27 08:15:45 -0700

    added in basically everything from the first gui project

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -1,6 +1,28 @@
 package view;
 
-public class Frame
+import javax.swing.JFrame;
+import controller.GUIController;
+
+public class Frame extends JFrame
 {
+	private GUIController appController;
+	private Panel appPanel;
+	
+	public Frame(GUIController appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new Panel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setTitle("My Second GUI");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
 
 }
--- src/view/Panel.java
+++ src/view/Panel.java
@@ -1,6 +1,66 @@
 package view;
 
-public class Panel
+import javax.swing.*;
+import controller.GUIController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel extends JPanel
 {
 
+	private GUIController appController;
+	private JButton myButton;
+	private JLabel myLabel;
+	private SpringLayout appLayout;
+	
+	public Panel(GUIController appController)
+	{
+		super();
+		this.appController = appController;
+		myButton = new JButton("Click");
+		myLabel = new JLabel("Words on a line");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.add(myButton);
+		this.add(myLabel);
+		
+	}
+	
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.NORTH, myLabel,74, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, myLabel,-86, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.WEST, myButton,174, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, myButton,-135, SpringLayout.SOUTH, this);
+	}
+	
+	private void setupListeners()
+	{
+		myButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeColor();
+			}
+			
+		});
+	}
+	
+	
+	private void changeColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+	}
 }
